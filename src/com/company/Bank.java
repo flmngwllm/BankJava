7cbec2dac93ea3bb8c161ded060f2be847d15fe1
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-02-25 20:03:32 -0500

    added test data

--- src/com/company/Bank.java
+++ src/com/company/Bank.java
@@ -6,61 +6,66 @@ public class Bank {
     private String name;
     private ArrayList<Branch> branches;
 
-    public Bank(String name){
+    public Bank(String name) {
         this.name = name;
         this.branches = new ArrayList<Branch>();
     }
 
-    public boolean addBranch(String branchName){
-        if(findBranch(branchName) == null){
+    public boolean addBranch(String branchName) {
+        if (findBranch(branchName) == null) {
             this.branches.add(new Branch(branchName));
             return true;
         }
         return false;
     }
 
-    public boolean addCustomer(String branchName, String customerName, double initialAmount){
+    public boolean addCustomer(String branchName, String customerName, double initialAmount) {
         Branch branch = findBranch(branchName);
-        if(branch != null){
+        if (branch != null) {
             return branch.newCustomer(customerName, initialAmount);
         }
         return false;
     }
 
-    public boolean addCustomerTransactions(String branchName, String customerName, double amount){
+    public boolean addCustomerTransactions(String branchName, String customerName, double amount) {
         Branch branch = findBranch(branchName);
-        if(branch != null){
+        if (branch != null) {
             return branch.addCustomerTransaction(customerName, amount);
         }
         return false;
     }
 
-    private Branch findBranch(String customerName){
-        for(int i =0; i<this.branches.size(); i++){
+    private Branch findBranch(String customerName) {
+        for (int i = 0; i < this.branches.size(); i++) {
             Branch checkedBranch = this.branches.get(i);
-            if(checkedBranch.getName().equals(customerName)){
+            if (checkedBranch.getName().equals(customerName)) {
                 return checkedBranch;
             }
         }
         return null;
     }
 
-    public boolean listCustomer(String branchName, boolean showTransactions){
+    public boolean listCustomer(String branchName, boolean showTransactions) {
         Branch branch = findBranch(branchName);
-        if(branch != null){
+        if (branch != null) {
             System.out.println("Customer details for branch " + branch.getName());
-            ArrayList<Customer>branchCustomers = branch.getCustomers();
-            for(int i = 0; i<branchCustomers.size(); i++){
+
+            ArrayList<Customer> branchCustomers = branch.getCustomers();
+            for (int i = 0; i < branchCustomers.size(); i++) {
                 Customer branchCustomer = branchCustomers.get(i);
                 System.out.println("Customer: " + branchCustomer.getName() + "[" + i + "]");
-                if(showTransactions){
+                if (showTransactions) {
                     System.out.println("Transactions");
-                    ArrayList<Double>transactions = branchCustomer.getTransactions();
-                    for(int j =0; j<transactions.size(); j++){
-                        System.out.println("[" + (j+i) + "] Amount " + transactions.get(j));
+                    ArrayList<Double> transactions = branchCustomer.getTransactions();
+                    for (int j = 0; j < transactions.size(); j++) {
+                        System.out.println("[" + (j + i) + "] Amount " + transactions.get(j));
                     }
                 }
             }
+            return true;
+
+        } else {
+            return false;
         }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+Bank bank = new Bank("American bank");
+
+bank.addBranch("Wells Fargo");
+bank.addCustomer("Wells Fargo,", "Will", 12.00);
+
+bank.addBranch("Bank of America");
+bank.addCustomer("Bank of America", "bob", 44.44);
+
+bank.listCustomer("Wells Fargo", false);
     }
 }
